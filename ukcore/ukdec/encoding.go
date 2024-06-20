5d8fe4ed8d21d9b32fc8f30f20b0ecb84206cf52
Oligarch E <oligarch@example.com>
2024-06-20 16:20:45 -0400

    Replace `ukreflect` usage in `ukdec`

--- ukcore/ukdec/encoding.go
+++ ukcore/ukdec/encoding.go
@@ -4,9 +4,9 @@ import (
 	"errors"
 	"reflect"
 
+	"github.com/oligarch316/go-ukase/ukcore"
 	"github.com/oligarch316/go-ukase/ukcore/ukexec"
 	"github.com/oligarch316/go-ukase/ukcore/ukspec"
-	"github.com/oligarch316/go-ukase/ukreflect"
 )
 
 type Decoder struct {
@@ -22,37 +22,37 @@ func NewDecoder(input ukexec.Input, opts ...Option) *Decoder {
 }
 
 func (d *Decoder) Decode(params any) error {
-	val, err := d.loadValue(params)
+	paramsVal, err := d.loadValue(params)
 	if err != nil {
 		return err
 	}
 
-	spec, err := d.loadSpec(val)
+	spec, err := d.loadSpec(paramsVal)
 	if err != nil {
 		return err
 	}
 
 	for _, flag := range d.input.Flags {
-		if err := d.decodeFlag(val, spec, flag); err != nil {
+		if err := d.decodeFlag(paramsVal, spec, flag); err != nil {
 			return err
 		}
 	}
 
-	return d.decodeArgs(val, spec, d.input.Args)
+	return d.decodeArgs(paramsVal, spec, d.input.Args)
 }
 
-func (Decoder) loadValue(v any) (reflect.Value, error) {
-	val, err := ukreflect.LoadValueOf(v)
+func (Decoder) loadValue(v any) (ukcore.ParamsValue, error) {
+	paramsVal, err := ukcore.NewParamsValue(v)
 	if err != nil {
-		val = reflect.ValueOf(v)
-		err = decodeErr(val).params(err)
+		tmpVal := reflect.ValueOf(v)
+		err = decodeErr(tmpVal).params(err)
 	}
 
-	return val, err
+	return paramsVal, err
 }
 
-func (d Decoder) loadSpec(structVal reflect.Value) (ukspec.Params, error) {
-	spec, err := ukspec.New(structVal.Type(), d.config.Spec...)
+func (d Decoder) loadSpec(paramsVal ukcore.ParamsValue) (ukspec.Params, error) {
+	spec, err := ukspec.New(paramsVal.Type(), d.config.Spec...)
 	if err != nil {
 		// TODO: Wrap error appropriately
 	}
@@ -60,26 +60,26 @@ func (d Decoder) loadSpec(structVal reflect.Value) (ukspec.Params, error) {
 	return spec, err
 }
 
-func (d Decoder) decodeFlag(structVal reflect.Value, spec ukspec.Params, flag ukexec.InputFlag) error {
+func (d Decoder) decodeFlag(paramsVal ukcore.ParamsValue, spec ukspec.Params, flag ukexec.InputFlag) error {
 	flagSpec, ok := spec.FlagIndex[flag.Name]
 	if !ok {
-		return decodeErr(structVal).flagName(flag)
+		return decodeErr(paramsVal.Value).flagName(flag)
 	}
 
-	fieldVal := ukreflect.LoadFieldByIndex(structVal, flagSpec.FieldIndex)
+	fieldVal := paramsVal.EnsureFieldByIndex(flagSpec.FieldIndex)
 
 	if err := decode(fieldVal, flag.Value); err != nil {
 		if errors.Is(err, errUnsupportedKind) {
-			return decodeErr(structVal).field(fieldVal, flagSpec.FieldName, err)
+			return decodeErr(paramsVal.Value).field(fieldVal, flagSpec.FieldName, err)
 		}
 
-		return decodeErr(structVal).flagValue(flag, err)
+		return decodeErr(paramsVal.Value).flagValue(flag, err)
 	}
 
 	return nil
 }
 
-func (d Decoder) decodeArgs(structVal reflect.Value, spec ukspec.Params, args []string) error {
+func (d Decoder) decodeArgs(paramsVal ukcore.ParamsValue, spec ukspec.Params, args []string) error {
 	switch {
 	case len(args) == 0:
 		return nil
@@ -87,7 +87,7 @@ func (d Decoder) decodeArgs(structVal reflect.Value, spec ukspec.Params, args []
 		return errors.New("[TODO decodeArgs] have args but not spec")
 	}
 
-	argsVal := ukreflect.LoadFieldByIndex(structVal, spec.Args.FieldIndex)
+	argsVal := paramsVal.EnsureFieldByIndex(spec.Args.FieldIndex)
 
 	for _, arg := range args {
 		if err := decode(argsVal, arg); err != nil {
